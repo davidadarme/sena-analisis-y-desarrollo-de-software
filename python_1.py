a0b0ddc1e2962aebc05616860af3075f88d73cd0
David Adarme <114038362+davidadarme@users.noreply.github.com>
2022-11-28 19:15:32 -0500

    Delete python_1.py

--- python_1.py
+++
@@ -1,5 +0,0 @@
-# 1. Escribi un programa que solicite al usuario que ingrese su nombre. El nombre se debe almacenar en una variable llamada nombre. 
-# A continuación, se debe mostrar en pantalla el texto "Ahora estás en la matrix, [usuario, donde "[usuario" se reemplazará por el nombre que el usuario haya ingresado. *
-
-nombre = (input("Ingrese su nombre: "))
-print("Ahora estas en la matrix", nombre)
