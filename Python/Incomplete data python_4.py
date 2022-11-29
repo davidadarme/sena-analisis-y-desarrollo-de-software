962db94b7c9e48d6f1504acea4d4c6f9950f38bc
David Adarme <114038362+davidadarme@users.noreply.github.com>
2022-11-28 20:13:20 -0500

    Create Incomplete data python_4.py

---
+++ Python/Incomplete data python_4.py
@@ -0,0 +1,8 @@
+#Escribi un programa que solicité al usuario ingresar la cantidad de kilómetros recorridos por una motocicleta y la cantidad de litros de combustible que consumió ddurante ese recorrido.  Mostrar el consumo de combustible por kilómetro.
+
+print("Bienvenido, ejercicio 4")
+km_data = float(input("Ingrese la cantidad de kilometros: "))
+gas_data = float(input("Ingrese la cantidad de combustible: "))
+total_data_value = km_data*gas_data
+
+print(total_data_value)
