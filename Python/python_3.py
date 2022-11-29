d7e4f62f8e6038cbe810246dd36d4764b263a73f
David Adarme <114038362+davidadarme@users.noreply.github.com>
2022-11-28 20:00:18 -0500

    Update python_3.py

--- Python/python_3.py
+++ Python/python_3.py
@@ -4,13 +4,14 @@
 # Por último, muestra en pantalla el resultado de la multiplicación de este nuevo número por el resultado de la suma anterior.
 
 
+#1
 print("Bienvenido, ejercicio 3")
 value_number1 = float(input("Ingrese un numero: "))
 value_number2 = float(input("Ingrese un segundo numero: "))
 total_value = value_number1+value_number2
-
 print("El resultado de su suma es: ", total_value)
 
+#2
 value_number3 = float(input("Ingrese un tercer numero: "))
 operation = value_number3*total_value
 print("El resultado de su operacion es: ",operation)
