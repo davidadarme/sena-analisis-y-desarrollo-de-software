a5e2fe6aa77847b64322f781dceb08c1bc291216
David Adarme <114038362+davidadarme@users.noreply.github.com>
2022-12-04 18:08:39 -0500

    Update exercise_py7.py

--- Python-exercises/exercise_py7.py
+++ Python-exercises/exercise_py7.py
@@ -2,20 +2,26 @@
 # Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de ahorros, introducida por el usuario. Después el programa debe calcular y mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. 
 
 '''
-interest rate +4%=0.04
 Calcular y mostrar por pantalla la cantidad de ahorros tras el primer
 '''
+
 print("Welcome to Nubank")
-username = str("Ingrese su nombre")
+
+#User
+username = input("Ingrese su nombre: ")
+print("Welcome back", username)
+
+#CashData
 cash_data = int(input("Ingrese la cantidad de dinero depositada: "))
 interest_rate = 0.04
-total_cash= cash_data (1+interest_rate)
+total_cash= cash_data * (1 + interest_rate)
 
-cash_data_year1 = cash_data* (1+ interest_rate)
-print("La cantidad de ahorros durante el primer año:", str(round(cash_data_year1)))
+#Proccess
+cash_data_year1 = cash_data * (1 + interest_rate)
+print("La cantidad de ahorros durante el primer año:", cash_data_year1)
 
-cash_data_year2 = cash_data* (1+ interest_rate)
-print("La cantidad de ahorros durante el primer año:", str(round(cash_data_year2, 2)))
+cash_data_year2 = cash_data * (1 + interest_rate*2)
+print("La cantidad de ahorros durante el primer año:", cash_data_year2)
 
-cash_data_year3 = cash_data* (1+ interest_rate)
-print("La cantidad de ahorros durante el primer año:", str(round(cash_data_year3)))
+cash_data_year3 = cash_data * (1+ interest_rate*3)
+print("La cantidad de ahorros durante el primer año:", cash_data_year3)
